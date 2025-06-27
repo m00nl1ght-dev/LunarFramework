e453a70b463e3900bd6e70303590dadafe4763e2
m00nl1ght <github@m00nl1ght.dev>
2025-06-27 14:51:27 +0200

    Initial support for RimWorld 1.6

--- Sources/LunarFramework/Internal/Patches/Patch_Verse_Widgets.cs
+++ Sources/LunarFramework/Internal/Patches/Patch_Verse_Widgets.cs
@@ -22,6 +22,9 @@ internal static class Patch_Verse_Widgets
         ToStringStyle valueStyle, float gap,
         GameFont sliderLabelFont,
         Color? sliderLabelColor,
+        #if RW_1_6_OR_GREATER
+        float roundTo,
+        #endif
         Func<float, string> toString)
     {
         IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -30,7 +33,11 @@ internal static class Patch_Verse_Widgets
                 .MatchLdarg().Remove()
                 .Match(OpCodes.Ldc_I4_1).Remove()
                 .MatchCall(typeof(GenText), nameof(GenText.ToStringByStyle)).Remove()
+                #if RW_1_6_OR_GREATER
+                .Insert(OpCodes.Ldarg, 11)
+                #else
                 .Insert(OpCodes.Ldarg, 10)
+                #endif
                 .Insert(CodeInstruction.Call(Self, nameof(ToStringCustom)))
                 .Lazy(2);
 
@@ -47,7 +54,11 @@ internal static class Patch_Verse_Widgets
         GameFont sliderLabelFont = GameFont.Tiny,
         Color? sliderLabelColor = null)
     {
+        #if RW_1_6_OR_GREATER
+        FloatRange_Custom(rect, id, ref range, min, max, labelKey, ToStringStyle.FloatTwo, gap, sliderLabelFont, sliderLabelColor, 0f, toString);
+        #else
         FloatRange_Custom(rect, id, ref range, min, max, labelKey, ToStringStyle.FloatTwo, gap, sliderLabelFont, sliderLabelColor, toString);
+        #endif
     }
 
     private static string ToStringCustom(float val, Func<float, string> func) => func(val);
